35ed0699d347cf6c04245c334feed136d746d54e
teovlt <teo.villet2@gmail.com>
2025-04-27 08:34:50 +0200

    Add basic landing page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_teo/pages/page_accueil.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Chti Facebook',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
+      home: const PageAccueil(),
     );
   }
 }
---
+++ lib/pages/page_accueil.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PageAccueil extends StatefulWidget {
+  const PageAccueil({super.key});
+
+  @override
+  State<PageAccueil> createState() => _PageAccueilState();
+}
+
+class _PageAccueilState extends State<PageAccueil> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Page d\'accueil'),
+      ),
+      body: Center(child: Text('Chti facebook')),
+    );
+  }
+}
