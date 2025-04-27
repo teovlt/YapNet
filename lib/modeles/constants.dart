41f82edfe0bdf8eb2c3f6e607b5af65eaa73923c
teovlt <teo.villet2@gmail.com>
2025-04-27 16:09:54 +0200

    Add constants file for models

---
+++ lib/modeles/constants.dart
@@ -0,0 +1,24 @@
+//Collection
+String memberCollectionKey = 'members';
+String postCollectionKey = "posts";
+String commentCollectionKey = 'comments';
+String notificationCollectionKey = 'notifications';
+
+//Member
+String memberIdKey = "memberID";
+String nameKey = 'name';
+String surnameKey = 'surname';
+String profilePictureKey = 'profilePicture';
+String coverPictureKey = 'coverPicture';
+String descriptionKey = 'description';
+
+//Post
+String textKey = 'text';
+String postImageKey = 'image';
+String dateKey = 'date';
+String likesKey = 'likes';
+
+//Notification
+String fromKey = 'from';
+String isReadKey = 'read';
+String postIdKey = 'postID';
