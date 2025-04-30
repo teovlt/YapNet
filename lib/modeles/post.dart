983aab4cfc18a93db8f0029f504f34b913008dfa
teovlt <teo.villet2@gmail.com>
2025-04-30 18:10:07 +0200

    Create model for post

---
+++ lib/modeles/post.dart
@@ -0,0 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Post {
+  DocumentReference reference;
+  String id;
+  Map<String, dynamic> map;
+
+  Post({required this.reference, required this.id, required this.map});
+
+  String get member => map['memberIdKey'] ?? '';
+  String get text => map['textKey'] ?? '';
+  String get imageUrl => map['postImageKey'] ?? '';
+  int get date => map['dateKey'] ?? 0;
+  List<dynamic> get likes => map['likesKey'] ?? [];
+}
